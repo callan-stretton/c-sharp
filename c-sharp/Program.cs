e0988fd3cba6b8b9e6a340c26980d71dd1187081
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-03-31 22:40:26 +1300

    using another if statement with ToUpper()

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -6,17 +6,26 @@ namespace csharp
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("I will multiply a number by 5! What's your number?"); 
-            string input = Console.ReadLine();
-            int x;
-            if (int.TryParse(input, out x))
+            Console.WriteLine("Would you like to multiply numbers? Y/N?");
+            string firstInput = Console.ReadLine().ToUpper();
+            if (firstInput == "Y")
             {
-                Console.WriteLine(x * 5);
+                Console.WriteLine("I will multiply a number by 5! What's your number?");
+                string secondInput = Console.ReadLine();
+                int x;
+                if (int.TryParse(secondInput, out x))
+                {
+                    Console.WriteLine(x * 5);
+                }
+                else
+                {
+                    Console.Beep();
+                    Console.WriteLine("Wow... " + secondInput + " is not a number!");
+                }
             }
             else
             {
-                Console.Beep();
-                Console.WriteLine("Wow... " + input + " is not a number!");
+                Console.WriteLine();
             }
         }
     }
